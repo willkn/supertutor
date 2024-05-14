ed73bb1d6c791c940704bc18d5c0ce24db1a4531
willkn <williamknott00@gmail.com>
2024-05-14 19:24:42 +0100

    intial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const OpenAI = require('openai');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json()); // Add this line to parse JSON bodies
+
+// Initialize OpenAI instance with your API key
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+const speechFilePath = path.resolve('./speech.mp3');
+
+// Function to read and encode the MP3 file
+async function encodeMP3() {
+  try {
+    const mp3Buffer = await fs.promises.readFile(speechFilePath);
+    return mp3Buffer.toString('base64');
+  } catch (error) {
+    console.error('Error reading MP3 file:', error);
+    throw error;
+  }
+}
+
+// Endpoint to generate chat completions
+app.post('/generate-text', async (req, res) => {
+  const { text } = req.body;
+
+  if (!text) {
+    return res.status(400).json({ error: 'Text is required' });
+  }
+
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: text}, {role: 'system', content: 'respond in less than 50 words'}],
+      model: 'gpt-4o',
+    });
+
+    const generatedText = completion.choices[0].message.content;
+
+    const mp3 = await openai.audio.speech.create({
+      model: 'tts-1',
+      voice: 'alloy',
+      input: generatedText,
+    });
+
+    // Write the MP3 file
+    const mp3Buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFilePath, mp3Buffer);
+
+    // Encode the MP3 file to Base64
+    const mp3Base64 = await encodeMP3();
+
+    // Send both the generated text and the Base64-encoded MP3 as response
+    res.json({ generatedText, mp3: mp3Base64 });
+  } catch (error) {
+    console.error('Error fetching response from OpenAI API:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
